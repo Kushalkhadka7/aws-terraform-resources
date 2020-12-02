8ba93ca80f0402fe22d2c338b82f595ced17013f
Kushalkhadka7 <crkushal7@gmail.com>
2020-12-03 00:00:10 +0545

    Fix issue on alb

--- asg.tf
+++ asg.tf
@@ -1,29 +1,71 @@
-resource "aws_autoscaling_group" "admin-asg" {
-  name                      = "admin-asg"
+resource "aws_autoscaling_group" "admin_asg" {
+  name                      = "admin_asg"
   availability_zones        = [data.aws_availability_zones.all.names]
   min_size                  = 1
   max_size                  = 3
   health_check_type         = "ELB"
   health_check_grace_period = 300
   desired_capacity          = 1
-  launch_configuration      = aws_launch_configuration.admin_asg_launch
+  launch_configuration      = aws_launch_configuration.admin_asg_launch.name
   load_balancers            = [aws_elb.load_balancer]
+  security_groups           = [aws_security_group.asg_security_group]
+
 
   tag {
-    Name                = "admin-asg"
+    Name                = "admin_asg"
     propagate_at_launch = true
   }
 }
 
 
 resource "aws_launch_configuration" "admin_asg_launch" {
-  image_id = "ami-0c55b159cbfafe1f0"
-
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.asg_security_group]
+  image_id          = data.aws_ami.ubuntu.id
+  instance_type     = "t2.micro"
+  security_groups   = [aws_security_group.ec2_security_group.id]
+  enable_monitoring = true
+  ebs_optimized     = true
 
   lifecycle {
     create_before_destroy = true
   }
+
+  ebs_block_device = [
+    {
+      device_name           = "/dev/xvdz"
+      volume_type           = "gp2"
+      volume_size           = "50"
+      delete_on_termination = true
+    },
+  ]
+
+  root_block_device = [
+    {
+      volume_size = "50"
+      volume_type = "gp2"
+    },
+  ]
 }
 
+
+resource "aws_autoscaling_policy" "admin_asg_policy" {
+  name                    = "admin_asg_policy"
+  scaling_adjustment      = 3
+  adjustment_type         = "ChangeInCapacity"
+  cooldown                = 300
+  policy_type             = "SimpleScaling"
+  auto_scaling_group_name = aws_autoscaling_group.admin_asg
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 40.0
+  }
+}
+
+
+resource "aws_autoscaling_attachment" "asg_admin_elb" {
+  autoscaling_group_name = aws_autoscaling_group.admin_asg.id
+  alb_target_group_arn   = aws_lb_target_group.alb_admin_target_group.arn
+}
--- elb.tf
+++ elb.tf
@@ -1,25 +1,19 @@
-resource "aws_elb" "load_balancer" {
+resource "aws_alb" "application_load_balancer" {
   name                        = "load_balancer"
+  internal                    = false
+  load_balancer_type          = "application"
   availability_zones          = [data.aws_availability_zones.all.names]
-  security_groups             = [aws_security_group.elb_security_group]
+  security_groups             = [aws_security_group.elb_security_group.id]
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
   connection_draining_timeout = 400
-  subnets                     = [aws_subnet.public_subnet]
+  subnets                     = aws_subnet.public_subnet.*.id
 
   access_logs {
-    enabled       = true
-    bucket        = "load_balancer"
-    bucket_prefix = "load_balancer/"
-    interval      = 60
-  }
-
-  listener {
-    lb_port           = 80
-    lb_protocal       = "http"
-    instance_port     = 80
-    instance_protocal = "http"
+    enabled = true
+    bucket  = "load_balancer"
+    prefix  = "load_balancer/"
   }
 
   health_check {
@@ -31,6 +25,52 @@ resource "aws_elb" "load_balancer" {
   }
 
   tags = {
-    Name = "load_balancer"
+    Name        = "load_balancer"
+    Enviornment = "production"
   }
+}
+
+
+resource "aws_alb_listener" "application_lb_listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn
+  port              = 80
+  protocol          = "http"
+}
+
+resource "aws_lb_listener_rule" "admin_rule" {
+  listener_arn = aws_lb_listener.application_lb_listener.arn
+
+  action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "HEALTHY"
+      status_code  = "200"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/admin/*"]
+    }
+  }
+
+  condition {
+    query_string {
+      key   = "health"
+      value = "check"
+    }
+
+    query_string {
+      value = "bar"
+    }
+  }
+}
+
+
+resource "aws_lb_target_group_attachment" "alb_admin_tg" {
+  target_group_arn = aws_lb_target_gaws_alb_target_grouproup.alb_admin_target_group.arn
+  target_id        = aws_instance.test.id
+  port             = 80
 }
